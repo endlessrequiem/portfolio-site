d39b970ee6e67672e93d07d97bf584b998ba1247
endlessrequiem <austin.j.white.96@gmail.com>
2025-05-16 10:53:04 -0700

    added resume card component

---
+++ src/components/ResumeCard/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+type ResumeCardProps = {
+    title: string;
+    listItems: string[];
+}
+
+
+export const ResumeCard: React.FC<ResumeCardProps> = ({title, listItems}) => {
+    return (
+        <view style={{border: "1px solid white", borderRadius: "10px", padding: "10px"}}>
+            <text>{title}</text>
+            <ul>
+                {listItems.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
+        </view>
+    )
+}
+
