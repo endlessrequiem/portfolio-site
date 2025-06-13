d515b8eda6e58e1eb9e204b504161b79b1fadbc2
endlessrequiem <austin.j.white.96@gmail.com>
2025-06-12 23:44:32 -0700

    add gh pages support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    assetPrefix: isProd ? '/portfolio-site/' : '',
+    basePath: isProd ? '/portfolio-site' : '',
+    // Other configurations
+};
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
 };
 
 export default nextConfig;
