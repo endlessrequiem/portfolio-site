ec6e92344deb207e4b41390b52642ed8f2e6e77d
endlessrequiem <austin.j.white.96@gmail.com>
2025-08-27 11:07:10 -0700

    remove unused import

--- src/components/ProjectSection/index.tsx
+++ src/components/ProjectSection/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, {useState} from "react";
-import {strings} from "@/app/page.strings";
 import {SectionHeader} from "@/components/SectionHeader";
 import {Pill} from "@/components/Pill";
 
