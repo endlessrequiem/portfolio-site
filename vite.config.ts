ded71a42e8dfb84400413d2360aab7309d6994e1
endlessrequiem <austin.j.white.96@gmail.com>
2025-09-29 22:17:35 -0700

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import {vitePlugin as remix} from "@remix-run/dev";
+import {defineConfig} from "vite";
 import path from "path";
 
-export default defineConfig(({ command }) => ({
-  plugins: [
-    remix({
-      ignoredRouteFiles: ["**/.*"],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./app"),
+export default defineConfig(({command}) => ({
+    plugins: [
+        remix({
+            ignoredRouteFiles: ["**/.*"],
+            ssr: false,
+        }),
+    ],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./app"),
+        },
     },
-  },
-  css: {
-    modules: {
-      localsConvention: "camelCase",
+    css: {
+        modules: {
+            localsConvention: "camelCase",
+        },
     },
-  },
-  base: command === "build" ? "/portfolio-site/" : "/",
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
-  },
+    base: command === "build" ? "/portfolio-site/" : "/",
+    build: {
+        outDir: "build",
+        emptyOutDir: true,
+    },
 }));
