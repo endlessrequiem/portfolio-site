a3c8126df4cc1abca4b2d1775352b4079118e6f2
endlessrequiem <austin.j.white.96@gmail.com>
2025-09-29 22:11:05 -0700

    update ssr for remix config

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
-  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-site/" : "/",
+    ignoredRouteFiles: ["**/.*"],
+    publicPath: process.env.NODE_ENV === "production" ? "/portfolio-site/" : "/",
+    ssr: false,
 };
