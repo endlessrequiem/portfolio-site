e5b5ecd98cb5c4f6f6bd6bf055cc20021606e476
endlessrequiem <austin.j.white.96@gmail.com>
2025-10-03 20:57:59 -0700

    finally fix icon for real this time

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export default function App() {
         <head>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="icon" href="/favicon.ico"/>
+            <link rel="icon" href="./favicon.ico"/>
             <Meta/>
             <Links/>
         </head>
