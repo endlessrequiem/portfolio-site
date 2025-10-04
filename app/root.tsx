2cb92e55fb02ba1ff6f2d18c247040455ada9b61
endlessrequiem <austin.j.white.96@gmail.com>
2025-10-03 19:48:58 -0700

    update links

--- app/root.tsx
+++ app/root.tsx
@@ -17,11 +17,11 @@ export const links: LinksFunction = () => {
     return [
         {
             rel: "icon",
-            href: "/app/resources/icon.svg?",
+            href: "/app/resources/icon.svg",
         },
         {
             rel: "stylesheet",
-            href: "/app/styles/globals.css?",
+            href: "/app/styles/globals.css",
         },
     ];
 };
