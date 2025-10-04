a92e563790cdacdc31e4d7a56e657a117b78d361
endlessrequiem <austin.j.white.96@gmail.com>
2025-10-03 21:09:20 -0700

    finally fix icon for real this time I think

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export default function App() {
         <head>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="icon" href={`${process.env.PUBLIC_URL || ""}/favicon.ico`}/>
+            <link rel="icon" href="./favicon.ico"/>
             <Meta/>
             <Links/>
         </head>
